a845395deccfb14ed171498889060365fa0fa48b
PeiChi Chu <pchu2@go.shoreline.com>
2021-03-17 17:47:50 +0800

    forloop practice

---
+++ forloop_prac/forloop_prac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forloop_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peggy");
+        }
+    }
+}
